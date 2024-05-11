e8a0e39c53f1071de6b654b0ca2d503e4249a25c
DrSkywalker <efrye3@daltonstate.edu>
2024-05-10 20:57:25 -0400

    Updates !

---
+++ src/main/java/com/lee/darkskyforge/DarkSkyForge.java
@@ -0,0 +1,18 @@
+package com.lee.darkskyforge;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+
+@Mod(DarkSkyForge.MODID)
+public class DarkSkyForge
+{
+
+    public static final String MODID = "darkskyforge";
+        private static final Logger LOGGER = LogUtils.getLogger();
+    public DarkSkyForge()
+    {
+
+    }
+}
